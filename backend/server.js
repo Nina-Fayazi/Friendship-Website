fab3d381c950e672934a52c2f42b5a8597d00d88
Nina Fayazi <fayazinina@gmail.com>
2026-07-13 16:18:24 +0200

    Connect MongoDB database successfully with updated password

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose'); 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB database connection established successfully"))
+  .catch(err => console.log("MongoDB connection error: ", err));
+
 
 app.get('/', (req, res) => {
-  res.send('Server is running smoothly!');
+  res.send('Server is running and Database is connected!');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
